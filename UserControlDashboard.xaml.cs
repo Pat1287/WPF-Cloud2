d989591a4e5129a605e5052ff98114c6e2a49dc7
Pat1287 <58340434+Pat1287@users.noreply.github.com>
2019-12-09 19:20:45 -0600

    WPF-Cloud2

---
+++ UserControlDashboard.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Lógica de interacción para UserControlDashboard.xaml
+    /// </summary>
+    public partial class UserControlDashboard : UserControl
+    {
+        MainWindow _coontext;
+        public UserControlDashboard(MainWindow context)
+        {
+            InitializeComponent();
+            _coontext = context;
+
+        }
+    }
+}
